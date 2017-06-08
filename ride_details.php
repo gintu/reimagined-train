37e3f2dac64a66bf42dae913c9f1914d6e3cd357
abhi1401 <abhishek.mvr@outlook.com>
2017-06-08 11:18:42 +0530

    Modifications in profile page. Incomplete

--- ride_details.php
+++ ride_details.php
@@ -49,6 +49,10 @@
       <ul class="nav navbar-nav navbar-right">
         <li><a href="home_join_ride.php" data-vivaldi-spatnav-clickable="1">Find a Ride</a></li>
       </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="profile.php" data-vivaldi-spatnav-clickable="1">You</a></li>
+      </ul>
+
     </div>
   </div>
 </nav>
@@ -56,13 +60,11 @@
       <div id="headp">
 
 
-  <h1>Your Profile</h1>
-  <p>Here are all your Detalis</p>
+  <h1>Ride#<?php echo $_GET['count'];?></h1>
+  <p>Here are all your Details</p>
 </div>
 </div>
 <div class="container">
-
-
 <?php
 
 session_start();
@@ -77,137 +79,64 @@ $conn = mysqli_connect($servername, $username, $password,"carpooldb");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-/*echo "Connected successfully";*/
-/*$uname=$_POST['u_name'];
-$email=$_POST['email'];
-$bdate=$_POST['bdate'];
-$pno=$_POST['pno'];
-$gender=$_POST['gender'];
-$bio=$_POST['bio'];
 
-
-$sql = "INSERT INTO user (u_name, email, bdate,pno,gender,bio) VALUES ('$uname', '$email', '$bdate','$pno','$gender','$bio')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-*/
-
-
-
-$sql = "SELECT * FROM users where oauth_uid=".$_SESSION['uid'];
-$result = $conn->query($sql);
-if ($result) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      /*  echo "<h1>"."name "."</h1>" . $row["u_name"]. "- email " . $row["email"]. " " . $row["bdate"]. "<br>";*/
-      echo "<blockquote>";
-      echo '<img src="'.$_SESSION['picture'].'" width="100px" >';
-      echo '<h1>'.$row["first_name"].' '.$row["last_name"].'</h1>';
-      echo "<p>".$row["email"]."</p>";
-      echo '<a href="create_profile.php" class = "btn btn-success">Update Your Profile</a>';
-      echo "</blockquote>";
-
-//  header('Content-type: image/jpg');
-      // echo $content;
-//      echo '<img src="data:image/jpeg;base64,'.base64_encode($content->load()) .'" />';
-
- //echo "< img src = ".$image_content." width=200 height=200 >";
-    }
-} else {
-    echo "0 results";
-}
-
-
-
-
+$dj_id=$_GET['dj_id'];
 //echo $_SESSION['uid'];
-$sql = "SELECT * FROM journey where uid=".$_SESSION['uid'];
+$sql = "SELECT * FROM journey where j_id=".$dj_id;
 $result = $conn->query($sql);
 $count=0;
         echo "
                 <br><br>
                 <div>
-                  <h1>Your Ride Offers</h1>
-                  <p>Here's are all your offers</p>
+                  <h1>Ride details</h1>
+                  <p></p>
                 </div>";
         // output data of each row
     while($row = $result->fetch_assoc()) {
 
       /*  echo "<h1>"."name "."</h1>" . $row["u_name"]. "- email " . $row["email"]. " " . $row["bdate"]. "<br>";*/
-      echo "<form method='post' action='ride_details.php?dj_id=".$row['j_id']."&count=".($count+1)."'>";
-      $count++;
       echo "<div style='float:left;margin: 0 0 50px 50px;'><blockquote>";
-      echo "<h3> Ride #".$count."</h3>";
       echo "<p>".$row["j_start"]."</p>";
       echo "<p>".$row["j_finish"]."<p>";
       echo "<p>".$row["j_date"]."<p>";
       echo "<p>".$row["j_time"]."<p>";
       echo "<p style='color:yellow;'> Seats Left :".$row["seats"]."<p>";
-      echo "<button class= 'btn btn-success' type='submit'>See more details</button></td></tr>";
-echo "</blockquote></div></form>";
+      echo "<form method='post' action='dropride.php?dj_id=".$dj_id."'><button type='submit' class='btn btn-warning'>Drop this ride</a></blockquote></div></form><br>";
 //  header('Content-type: image/jpg');
       // echo $content;
 //      echo '<img src="data:image/jpeg;base64,'.base64_encode($content->load()) .'" />';
 
  //echo "< img src = ".$image_content." width=200 height=200 >";
  
-} 
-?>
 
-</div>
-
-      <div class="container">
-<?php
-$sql = "SELECT booked.seats as seats,j_start,j_finish,j_date,j_time,jid from booked,journey where booked.uid=".$_SESSION['uid']." and booked.jid = journey.j_id";
-$result = $conn->query($sql);
-$count=0;
-    echo "<h1>Your purchased rides </h1>
-        <p>Here's are all your rides</p>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      /*  echo "<h1>"."name "."</h1>" . $row["u_name"]. "- email " . $row["email"]. " " . $row["bdate"]. "<br>";*/
-      $count++;
-      echo "<form method = 'post' action='cancel_ride.php?cj_id=".$row['jid']."'>";
-      echo "<div style='float:left;margin: 0 0 50px 50px;'><blockquote>";
-      echo "<h3> Ride #".$count."</h3>";
-      echo "<p>".$row["j_start"]."</p>";
-      echo "<p>".$row["j_finish"]."<p>";
-      echo "<p>".$row["j_date"]."<p>";
-      echo "<p>".$row["j_time"]."<p>";
-      echo "<p style='color:cyan;'>Reserved ".$row["seats"]." seat<p>";      
-      echo "<button class= 'btn btn-success' type='submit'>Cancel Purchase</button></td></tr><br/>";
-echo "</blockquote></div></form>";
-/*
-$sql = "SELECT * FROM book,journey where book.u_id='31'";
-$result = $conn->query($sql);
-$count=0;
-if ($result) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      /*  echo "<h1>"."name "."</h1>" . $row["u_name"]. "- email " . $row["email"]. " " . $row["bdate"]. "<br>";
-      $count++;
-      echo "<h1> Ride #".$count."</h1>";
-      echo "<p>".$row["j_start"]."</p>";
-      echo "<p>".$row["j_finish"]."<p>";
-      echo "<p>".$row["j_date"]."<p>";
-      echo "<p>".$row["j_time"]."<p>";
-
-//  header('Content-type: image/jpg');
-      // echo $content;
-//      echo '<img src="data:image/jpeg;base64,'.base64_encode($content->load()) .'" />';
-
- //echo "< img src = ".$image_content." width=200 height=200 >";
-
-
-  */  }
-
-$conn->close();
-
+}
+      echo "<div style='clear:float;'><br><h2>Booked users<h2>";
+      $sqli = 'select first_name,bio,booked.seats as seats from booked,journey,users where journey.j_id = '.$dj_id.' and booked.jid=journey.j_id and users.oauth_uid = booked.uid';
+      $abc = $conn->query($sqli);
+      if($abc)
+      {
+        echo '
+          <tr>
+              <th>Sl. No</th>          
+              <th>Name</th>
+              <th>Bio</th>
+              <th>Seats booked</th>
+              <th></th>
+          </tr>';
+          $cnt = 1;
+      while( $row = $abc->fetch_assoc()) {
+            echo "<tr><td> ". $cnt++."<td> ". $row['first_name']. "</td><td> ". $row['bio']. "</td><td> ". $row['seats']. "</td></tr><br/>";
+         }
+      }
+      else
+        {
+        echo "<br/><blockquote><h4 class='text-success'>&lt/&gt &nbsp&nbspThere's nothing here... <h4></blockquote> ";
+      } 
 ?>
 </div>
+</div>
+
+
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
